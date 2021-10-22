c35c2180d90be321a0724908c1657452c6deba09
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-10-22 18:50:30 +0400

    uptade

---
+++ 18Oct2021/18Oct2021/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18Oct2021
+{
+    class Student
+    {
+        string name;
+        int age;
+        int group;
+
+        public Student(string name, int age, int group)
+        {
+            this.name = name;
+            this.group = group;
+            this.age = age;
+        }
+
+
+        public void addStd()
+        {
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Group: ");
+            int group = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Name:{name}, Age:{age}, Group:{group}");
+
+        }
+
+        public int GetBirthYear()
+        {
+            DateTime date = DateTime.Today;
+
+            return date.Year - this.age;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] std = new Student[2];
+            for (int i = 0; i < std.Length; i++)
+            {
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Group: ");
+                int group = Convert.ToInt32(Console.ReadLine());
+
+                
+
+            }
+
+            //Student std = new Student("",0,0);          
+            //std.addStd();
+            //Console.WriteLine(std.GetBirthYear());
+
+
+        }
+    }
+}
+
+#region
+
+//Student std = new Student("",0,0);          
+//std.addStd();
+//Console.WriteLine(std.GetBirthYear());
+
+
+//class Student
+//{
+//    string name;
+//    int group;
+//    int age;
+
+//    public Student(string name, int group, int age)
+//    {
+//        this.name = name;
+//        this.group = group;
+//        this.age = age;
+
+
+//    }
+
+//    public int GetBirthYear()
+//    {
+//        DateTime date = DateTime.Today;
+
+//        return date.Year - this.age;
+//    }
+//   
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            //ArrayList studentList = new ArrayList();
+//            //studentList.Add(student2);
+//            //studentList.Add(21);
+//            //studentList.Add(2000);
+//            //foreach (var item in studentList)
+//            //{
+//            //    Console.WriteLine(item);
+//            //}
+
+
+//            Student student = new Student("Sadiq", 434, 19);
+//            Student student2 = new Student("Xeyal", 208, 21);
+//            Student student3 = new Student("Elsen", 208, 23);
+//            Student student4 = new Student("Nurlan", 506, 25);
+//            Student student5 = new Student("Rasim", 434, 24);
+
+//            student.LIST();   
+//            student2.LIST();
+//            student3.LIST();
+//            student4.LIST();
+//            student5.LIST();
+
+
+//            Console.WriteLine(student2.GetBirthYear());
+//        }
+//    }
+
+//}
+#endregion
---
+++ 22Oct2021/Task/Task/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Task
+{
+    class Account
+    {
+        public string username;
+        public string password;
+        public int no;
+        static int code;
+
+        public Account(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+            no = ++code;
+        }
+
+        public static int IsPassWordvalid(string password)
+        {
+            int minLength = 8;
+            int maxLength = 25;
+
+            
+
+            bool Uppercase = password.Any(char.IsUpper);
+            bool Lowercase = password.Any(char.IsLower);
+            bool SpecialChar = password.Any(ch => !Char.IsLetterOrDigit(ch));
+            bool Digit = password.Any(char.IsDigit);
+
+            int score = 0;
+
+            if (password.Length >= minLength && maxLength >= password.Length)
+            {
+                score++;
+                Console.WriteLine("Minimum Length test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Minimum Length test failed.");
+            }
+
+            if (Uppercase)
+            {
+                score++;
+                Console.WriteLine("Uppercase test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Uppercase test failed.");
+            }
+
+            if (Lowercase)
+            {
+                score++;
+                Console.WriteLine("Lowercase test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Lowercase test failed.");
+            }
+
+            if (Digit)
+            {
+                score++;
+                Console.WriteLine("Digits test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Digits test failed.");
+            }
+
+            if (SpecialChar)
+            {
+                score++;
+                Console.WriteLine("Special Characters test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Special Characters test failed.");
+            }
+
+            return score;
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Username: ");
+            string username=Console.ReadLine();
+
+            
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+
+
+            static bool IsUserNameValid(string str)
+            {
+                {
+                    foreach (char c in str)
+                    {
+                        if (!Char.IsLetterOrDigit(c))
+                            return false;
+                    }
+                    return true;
+                }
+            }
+
+            if (IsPassWordvalid(password)==5 && IsUserNameValid(username))
+            {
+                Account account = new Account(username, password);
+                Console.WriteLine($"{account.no},{ account.username},{ account.password}");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            
+            static int IsPassWordvalid(string password)
+            {
+                int minLength = 8;
+                int maxLength = 25;
+
+
+
+                bool Uppercase = password.Any(char.IsUpper);
+                bool Lowercase = password.Any(char.IsLower);
+                bool SpecialChar = password.Any(ch => !Char.IsLetterOrDigit(ch));
+                bool Digit = password.Any(char.IsDigit);
+
+                int score = 0;
+
+                if (password.Length >= minLength && maxLength >= password.Length)
+                {
+                    score++;
+                    Console.WriteLine("Minimum Length test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Minimum Length test failed.");
+                }
+
+                if (Uppercase)
+                {
+                    score++;
+                    Console.WriteLine("Uppercase test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Uppercase test failed.");
+                }
+
+                if (Lowercase)
+                {
+                    score++;
+                    Console.WriteLine("Lowercase test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Lowercase test failed.");
+                }
+
+                if (Digit)
+                {
+                    score++;
+                    Console.WriteLine("Digits test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Digits test failed.");
+                }
+
+                if (SpecialChar)
+                {
+                    score++;
+                    Console.WriteLine("Special Characters test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Special Characters test failed.");
+                }
+
+                return score;
+            }
+        }
+    }
+
+}
