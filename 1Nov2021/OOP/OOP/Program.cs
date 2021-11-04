c3fcb00fec6c2f457cb4b8b7a5c79007ed527833
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-11-05 01:40:07 +0400

    uptade

---
+++ 1Nov2021/OOP/OOP/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User.ShowInfo();
+            User.PasswordChecker();
+        }
+    }
+
+
+    abstract class Account
+    {
+        public virtual void PasswordChecker()
+        {
+            Console.WriteLine("");
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+
+    class User : Account
+    {
+        public string fullname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+
+
+        public User(string fullname, string email, string password)
+        {
+            this.fullname = fullname;
+            this.email = email;
+            this.password = password;
+        }
+        public static List <User> user = new List<User>();
+
+        public static void ShowInfo()
+        {
+       
+
+            Console.WriteLine("Fullname daxil edin: ");
+            string fullname = Console.ReadLine();
+
+            Console.WriteLine("Email daxil edin: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("password daxil edin: ");
+            string password = Console.ReadLine();
+
+            user.Add(new User(fullname, email, password));
+
+            foreach (var item in user)
+            {
+                Console.WriteLine($"FullName:{item.fullname} email:{item.email} password:{item.password}");
+            }
+
+        }
+
+        public static void PasswordChecker()
+        {
+
+            foreach (var item in user)
+            {
+                int minLength = 8;
+                bool Uppercase = item.password.Any(char.IsUpper);
+                bool Lowercase = item.password.Any(char.IsLower);
+
+                int score = 0;
+
+
+
+                if (item.password.Length >= minLength)
+                {
+                    score++;
+                    Console.WriteLine("Minimum Length test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Minimum Length test failed.");
+                }
+
+                if (Uppercase)
+                {
+                    score++;
+                    Console.WriteLine("Uppercase test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Uppercase test failed.");
+                }
+
+                if (Lowercase)
+                {
+                    score++;
+                    Console.WriteLine("Lowercase test passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Lowercase test failed.");
+                }
+
+                if (score == 3)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }
+            
+
+        }
+    }
+}
