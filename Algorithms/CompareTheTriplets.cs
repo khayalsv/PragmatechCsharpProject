93091894f8db84a62def641f22ae6e9ad153debf
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-11-13 14:26:27 +0400

    uptade

---
+++ Algorithms/CompareTheTriplets.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class Result
+    {
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int alice = 0;
+            int bob = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    alice++;
+                }
+                else if (a[i] < b[i])
+                {
+                    bob++;
+                }
+            }
+            return new List<int> { alice, bob };
+        }
+
+    }
+}
