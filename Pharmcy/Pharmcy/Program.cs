1fd66fd9627ca4d2b73f21fd3722202e1259eb11
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-11-19 16:09:10 +0400

    uptade

---
+++ Pharmcy/Pharmcy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pharmcy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
