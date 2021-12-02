6783c89e9a6f2fa8632b99a950dd4ec240d6ca42
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-12-03 03:21:40 +0400

    uptade

---
+++ ExtentionMethod/ExtentionMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtensionMethod check = IsOdd;
+            check += IsEven;
+
+            Console.WriteLine(check.Invoke(20));
+            
+        }
+
+        public delegate bool ExtensionMethod(int n);
+
+        static void IsOdd(int n) => Console.WriteLine(n%2==1 ? "odd" : "even");;
+
+        
+
+
+    }
+
+
+}
---
+++ WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students;
+        public Form1()
+        {
+            students = new List<Student>();
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string fname = txtName.Text.Trim();
+            string fsurname = txtSurname.Text.Trim();
+            string femail = txtEmail.Text.Trim();
+
+            try
+            {
+                Student student = new Student
+                {
+                    name = fname,
+                    surname = fsurname,
+                    email = femail
+                };
+                students.Add(student);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Pls fill valid values for student", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            FillStudentList();
+        }
+        private void FillStudentList()
+        {
+            listStudent.Items.Clear();
+            foreach (var item in students)
+            {
+                listStudent.Items.Add(item.fullname+" "+item.email);
+
+            }
+        }
+    }
+}
