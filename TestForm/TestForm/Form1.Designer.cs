6f6c7fecae9e0664d0e1b592170ffd70f1d965c9
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-12-13 14:43:37 +0400

    uptade

--- TestForm/TestForm/Form1.Designer.cs
+++ TestForm/TestForm/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace StudentWinform
+namespace TestForm
 {
     partial class Form1
     {
@@ -31,17 +31,15 @@ namespace StudentWinform
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtSurname = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.cmbGroup = new System.Windows.Forms.ComboBox();
             this.dgv = new System.Windows.Forms.DataGridView();
-            this.label5 = new System.Windows.Forms.Label();
+            this.cmbGroup = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.uptadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -49,87 +47,44 @@ namespace StudentWinform
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(57, 167);
+            this.label1.Location = new System.Drawing.Point(37, 207);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Name\r\n";
+            this.label1.Text = "Name";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(60, 214);
+            this.txtName.Location = new System.Drawing.Point(40, 253);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(100, 20);
             this.txtName.TabIndex = 1;
             // 
-            // label2
+            // dgv
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(304, 167);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(7, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "\r\n";
-            // 
-            // txtSurname
-            // 
-            this.txtSurname.Location = new System.Drawing.Point(279, 214);
-            this.txtSurname.Name = "txtSurname";
-            this.txtSurname.Size = new System.Drawing.Size(100, 20);
-            this.txtSurname.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(276, 167);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Surname";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(536, 174);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 67);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(57, 52);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Select Group";
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(0, 323);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(800, 115);
+            this.dgv.TabIndex = 2;
             // 
             // cmbGroup
             // 
             this.cmbGroup.FormattingEnabled = true;
-            this.cmbGroup.Location = new System.Drawing.Point(60, 95);
+            this.cmbGroup.Location = new System.Drawing.Point(40, 127);
             this.cmbGroup.Name = "cmbGroup";
             this.cmbGroup.Size = new System.Drawing.Size(121, 21);
-            this.cmbGroup.TabIndex = 8;
+            this.cmbGroup.TabIndex = 4;
             // 
-            // dgv
+            // button1
             // 
-            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(0, 247);
-            this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(800, 191);
-            this.dgv.TabIndex = 11;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(169, 448);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "label5";
+            this.button1.Location = new System.Drawing.Point(576, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 69);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // menuStrip1
             // 
@@ -139,7 +94,7 @@ namespace StudentWinform
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // deleteToolStripMenuItem
@@ -147,33 +102,56 @@ namespace StudentWinform
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
             this.deleteToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
             this.deleteToolStripMenuItem.Text = "Delete";
-            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
             // 
             // uptadeToolStripMenuItem
             // 
             this.uptadeToolStripMenuItem.Name = "uptadeToolStripMenuItem";
             this.uptadeToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
             this.uptadeToolStripMenuItem.Text = "Uptade";
-            this.uptadeToolStripMenuItem.Click += new System.EventHandler(this.uptadeToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Select Group";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(279, 253);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Surname";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.dgv);
-            this.Controls.Add(this.cmbGroup);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtSurname);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbGroup);
+            this.Controls.Add(this.dgv);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -186,17 +164,15 @@ namespace StudentWinform
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtSurname;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox cmbGroup;
         private System.Windows.Forms.DataGridView dgv;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbGroup;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem uptadeToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label3;
     }
 }
 
---
+++ TestForm/TestForm/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        private List<Group> school = new List<Group>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            Student s1 = new Student("Khayal", "Salimov");
+            Student s2 = new Student("Sadiq", "Ilyasli");
+            Student s3 = new Student("Rasim", "Aliyev");
+            Student s4 = new Student("Nurlan", "Hasanli");
+
+            Group group1 = new Group("A101");
+            Group group2 = new Group("B255");
+
+            school.Add(group1);
+            school.Add(group2);
+            
+            group1.GroupAddStudent(s1);
+            group1.GroupAddStudent(s2);
+            group2.GroupAddStudent(s3);
+            group2.GroupAddStudent(s4);
+
+            cmbGroup.Items.AddRange(school.ToArray());
+            cmbGroup.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            string surname = txtSurname.Text.Trim();
+
+            if (name == "" || surname == "")
+            {
+                MessageBox.Show("Doldur", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Student student = new Student(name,surname);
+            Group selectGroup = (Group)cmbGroup.SelectedItem;
+            selectGroup.GroupAddStudent(student);
+
+            dgv.DataSource = null;
+            dgv.DataSource = selectGroup.GetAllStudent();
+
+            txtName.Text = "";
+            txtSurname.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
