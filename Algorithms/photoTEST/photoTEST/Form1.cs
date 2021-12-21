5cd3f0ed258b66a0a578ec8afbef3770d914ca59
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-12-21 22:58:55 +0400

    uptade

---
+++ Algorithms/photoTEST/photoTEST/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace photoTEST
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> room;
+        public Form1()
+        {
+            room = new List<Student>();
+            InitializeComponent();
+        }
+
+        private void selectListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectList list = new SelectList(room);
+            list.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+   
+
+            string name = txtName.Text.Trim();
+            string surname = txtSurname.Text.Trim();
+            string email = txtEmail.Text.Trim();
+
+            try
+            {
+                Student student = new Student
+                {
+                    Name = name,
+                    Surname = surname,
+                    Email = email
+                };
+                room.Add(student);
+                MessageBox.Show("Elave olundu", "Success", MessageBoxButtons.OK);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Yeniden yaz", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            txtName.Text = "";
+            txtSurname.Text = "";
+            txtEmail.Text = "";
+
+        }
+    }
+}
---
+++ CalculatorFom/calculatorFom/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorFom
+{
+    public partial class Form1 : Form
+    {
+        public double first { get; private set; }
+        public double second { get; private set; }
+
+        public string method { get; private set; }
+        public double result { get; private set; }
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//1
+        {
+            if (box.Text == "0" && box.Text != null)
+            {
+                box.Text = "1";
+            }
+            else
+            {
+                box.Text = box.Text + "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//2
+        {
+            if (box.Text == "0" && box.Text != null)
+            {
+                box.Text = "2";
+            }
+            else
+            {
+                box.Text = box.Text + "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)//3
+        {
+            box.Text = box.Text + "3";
+        }
+
+        private void button10_Click(object sender, EventArgs e) //++++++++++++++++
+        {
+            first = Convert.ToDouble(box.Text);
+            box.Text = "0";
+            method = "+";
+        }
+
+        private void button11_Click(object sender, EventArgs e) //----------------
+        {
+            first = Convert.ToDouble(box.Text);
+            box.Text = "0";
+            method = "-";
+
+
+        }
+
+        private void button12_Click(object sender, EventArgs e) //==================
+        {
+            
+            second = Convert.ToDouble(box.Text);
+
+
+            if (method == "+")
+            {
+                result = (first + second);
+                box.Text = Convert.ToString(result);             
+            }
+
+            if (method == "-")
+            {
+                result = (first - second);
+                box.Text = Convert.ToString(result);
+            }
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+  
+    }
+}
