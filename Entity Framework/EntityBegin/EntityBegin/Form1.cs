2fc8d684812484f3fd98180522ae3721b53346d3
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-01-20 01:00:54 +0400

    update

---
+++ Entity Framework/EntityBegin/EntityBegin/Form1.cs
@@ -0,0 +1,45 @@
+﻿using EntityBegin.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityBegin
+{
+    public partial class Form1 : Form
+    {
+        private readonly AkademiyaEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new AkademiyaEntities();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbGroup.DataSource = db.Groups.Select(x => x.Name).ToList(); //db-den groupu comboboxa cekir
+        }
+
+        private void cmbGroup_SelectedIndexChanged(object sender, EventArgs e) 
+        {
+            string group = cmbGroup.Text;
+            if (!string.IsNullOrEmpty(group)) //comboboxda ad ferqli olarsa cxarmaz studenti tapmaz
+            {
+                Group groupresult = db.Groups.FirstOrDefault(x => x.Name == group); //gelen soz groupda tapilarse
+                if (groupresult.Students.Count==0)
+                {
+                    MessageBox.Show("Null");
+                }
+                cmbStudent.DataSource = groupresult.Students.Select(x => x.Name).ToList();
+            }
+        }
+
+        
+    
+    }
+}
---
+++ Entity Framework/EntityCRUD/EntityCRUD/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityCRUD
+{
+    public partial class Form1 : Form
+    {
+        private readonly DBEntities db;
+        Customer model = new Customer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DBEntities();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            DataView();
+        }
+
+
+        void Clear()
+        {
+            txtFname.Text = txtLname.Text = txtCity.Text = txtAddress.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.CustomerID = 0;
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            model.FirstName = txtFname.Text.Trim();
+            model.LastName = txtLname.Text.Trim();
+            model.City = txtCity.Text.Trim();
+            model.Address = txtAddress.Text.Trim();
+
+
+            if (model.CustomerID == 0) //insert
+                db.Customers.Add(model);
+            else //update
+                db.Entry(model).State = EntityState.Modified;
+            db.SaveChanges();
+
+           
+
+            Clear();
+            DataView();
+            MessageBox.Show("Submit Successfuly");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void DataView()
+        {
+            dtGrid.AutoGenerateColumns = false; //2ci viewni gizledir
+            dtGrid.DataSource = db.Customers.ToList<Customer>();
+        }
+
+        private void dtGrid_DoubleClick(object sender, EventArgs e)//////////UPDATE
+        {
+            if (dtGrid.CurrentRow.Index !=-1)
+            {
+                model.CustomerID = Convert.ToInt32(dtGrid.CurrentRow.Cells["CustomerID"].Value);
+                model = db.Customers.Where(x => x.CustomerID == model.CustomerID).FirstOrDefault();
+                txtFname.Text = model.FirstName;
+                txtLname.Text = model.LastName;
+                txtCity.Text = model.City;
+                txtAddress.Text = model.Address;
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you Sure to Delete?","EF Crud Operation",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                var entry = db.Entry(model);
+                if (entry.State == EntityState.Deleted)
+                    db.Customers.Attach(model);
+
+                db.Customers.Remove(model);
+                db.SaveChanges();
+                DataView();
+                Clear();
+                MessageBox.Show("Deleted");
+                
+            }
+        }
+    }
+}
