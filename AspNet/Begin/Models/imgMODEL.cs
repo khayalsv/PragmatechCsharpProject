e9d4a99b44e8559feabab17435a902b6d069bf29
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-01 19:05:58 +0400

    update

---
+++ AspNet/Begin/Models/imgMODEL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Begin.Models
+{
+    public static class imgMODEL
+    {
+        public static List<imgItem> images = new List<imgItem>
+        {
+            new imgItem{ID=1,Title="Cloud",Link="1.jpeg"},
+            new imgItem{ID=2,Title="Cloud",Link="2.jpeg"},
+            new imgItem{ID=3,Title="Cloud",Link="3.jpeg"},
+            new imgItem{ID=4,Title="Cloud",Link="4.jpeg"},
+            new imgItem{ID=5,Title="Cloud",Link="5.jpeg"},
+            new imgItem{ID=6,Title="Cloud",Link="6.jpeg"},
+
+        };
+
+        public static List<imgItem> GetimgItem() => images;
+    }
+
+    public class imgItem
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+}
