2d8fa77d892a405bdee6e4396e6ebe29bb4722e4
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-03 20:52:23 +0400

    update

---
+++ KeyValue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>(); 
+            dictionary.Add(1, "Khayal");
+            dictionary.Add(2, "Sadiq");
+
+            dictionary.ToList().ForEach(x => Console.WriteLine(x.Key+" "+x.Value));
+
+          
+        }
+    }
+}
