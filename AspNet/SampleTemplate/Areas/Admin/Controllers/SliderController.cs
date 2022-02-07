9f605df4d593cabd8918430f7a6fe9629567d815
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-07 15:17:44 +0400

    update

---
+++ AspNet/SampleTemplate/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleTemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleTemplate.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly PortoDbContext dbContext;
+        public SliderController(PortoDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(dbContext.Sliders.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormFile photo,int price,string trend,string discount,string cloth)
+        {
+            Slider slider = new Slider
+            {
+                TrendWord = trend,
+                ClothType = cloth,
+                Price = price,
+                DisCount = discount,
+                Image = photo.FileName
+            };
+            dbContext.Sliders.Add(slider);
+            dbContext.SaveChanges();
+
+            return Redirect("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+    }
+}
