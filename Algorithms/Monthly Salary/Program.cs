d67eb0be6f9a868685847d2c93d52aa18d2d9efd
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-09 16:37:20 +0400

    update

---
+++ Algorithms/Monthly Salary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Monthly_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the Hourly Income");
+            //int hourlyincome = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the Weekly working hours");
+            //int weeklyworkhours = Int32.Parse(Console.ReadLine());
+
+            //int weeklyincome = hourlyincome * weeklyworkhours;
+
+            //int monthlyincome = weeklyincome * 4;
+
+            //Console.WriteLine("Salary: " + monthlyincome);
+
+
+            //
+
+            Console.WriteLine("Enter the Hourly Income");
+            int hourlyincome = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Weekly working hours");
+            int dailyworkhours = Int32.Parse(Console.ReadLine());
+
+            int dailyincome = hourlyincome * dailyworkhours;
+
+            int monthlyincome = dailyincome * 30;
+
+            Console.WriteLine("Salary: " + monthlyincome);
+
+
+        }
+    }
+}
---
+++ Algorithms/Square and Cube of the sum of number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Square_and_Cube_of_the_sum_of_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First number: ");
+            int a= Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            //Total
+            int total = a + b;
+
+            //Square
+            int square = total * total;
+
+            //Cube
+            int cube = square * total;
+
+            Console.WriteLine("Total: " + total);
+
+            Console.WriteLine("Square: " + square);
+
+            Console.WriteLine("Cube: " + cube);
+        }
+    }
+}
