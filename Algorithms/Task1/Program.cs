fa054e9a13ee5229022d63869421dc190d47fb6e
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-10 13:37:36 +0400

    update

---
+++ Algorithms/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Buy egg: ");
+            //int egg = Int32.Parse(Console.ReadLine());
+
+            //int pay = egg * 1;
+
+            //int healthy = egg - 5;
+
+            //int sell = healthy * 3;
+
+            //int gain = sell - pay;
+
+            //Console.WriteLine("Gain " + gain);
+
+
+
+
+
+            Console.WriteLine("Price: ");
+            int x = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Simit: ");
+            int y = Int32.Parse(Console.ReadLine());
+
+            int pay = x * y;
+            Console.WriteLine("Pay: " + pay);
+
+            Console.WriteLine("Eaten Simit: ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int remain = y - k;
+
+            int increasing = x + 1;
+
+            int sale = remain * increasing;
+
+            Console.WriteLine("Sale: " + sale);
+
+            int gain = sale - pay;
+
+            Console.WriteLine("Gain: " + gain);
+           
+            
+
+
+
+        }
+    }
+}
---
+++ Guessing Game/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        public static object DropDownList1 { get; private set; }
+
+        static void Main(string[] args)
+        {
+            //string secretWord = "salam";
+            //string guess = "";
+            //int guessCount = 0;
+            //int guessLimit = 3;
+            //bool outOfGuesses = false;
+
+            //while (guess !=secretWord && !outOfGuesses)
+            //{
+            //    if (guessCount<guessLimit)
+            //    {
+            //        Console.WriteLine("Enter guess: ");
+            //        guess = Console.ReadLine();
+            //        guessCount++;
+            //    }
+            //    else
+            //    {
+            //        outOfGuesses = true;
+            //    }
+
+            //}
+            //if (outOfGuesses)
+            //{
+            //    Console.WriteLine("You Lose!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You Win");
+            //}
+
+            //if (guessCount==1)
+            //{
+            //    Console.WriteLine("Skor: 6");
+            //}
+            //else if (guessCount == 2)
+            //{
+            //    Console.WriteLine("Skor: 4");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Skor: 2");
+            //}
+
+            //Console.WriteLine($"Yoxlanilan sozlerin sayi: {guessCount}");
+
+
+            //int guess = 0;
+            //int guessCount = 0;
+            //int secretNumber;
+
+            //Random random = new Random();
+            //secretNumber = random.Next(1, 10);
+
+            //while (guess != secretNumber)
+            //{
+            //    Console.WriteLine("Enter the number: ");
+            //    guess = Convert.ToInt32(Console.ReadLine());
+            //    guessCount++;
+
+            //    if (guess>secretNumber)
+            //    {
+            //        Console.WriteLine("Little number");
+            //    }
+            //    else if (guess<secretNumber)
+            //    {
+
+            //        Console.WriteLine("Big number");
+
+            //    }        
+            //}
+            //Console.WriteLine("Congrulation !");
+            //Console.WriteLine("{0} find the number ",guessCount);
+
+
+            //string words = "This is really fun. I am learning to code. ";
+
+            //char[] list = new char[] {' ', '.' };
+
+            //Random random = new Random();
+
+
+            //foreach (var item in words.Split(list, StringSplitOptions.RemoveEmptyEntries))
+            //{
+            //    //Console.WriteLine(item.ToCharArray());
+            //    Console.WriteLine(item[random.Next(0, item.Length)]);
+            //}
+
+
+
+            //Random random = new Random();
+
+            //string[] words = { "man", "rat", "cow", "chicken" };
+
+            //string word = words[random.Next(0, words.Length)];
+
+            //Console.WriteLine(words);
+
+
+
+
+
+        }
+
+
+    }
+}
