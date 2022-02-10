41d39f4f0867974b26943d019a9e03d6a5ad1f56
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-11 03:05:00 +0400

    update

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -1,128 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Timers;
 
 namespace Guessing_Game
 {
     class Program
     {
-        public static object DropDownList1 { get; private set; }
+        public static Timer aTmr = new Timer(1000);
+        public static int secondsCount = 0;
+        public static int total = 0;
+        public static int correctCount = 0;
 
         static void Main(string[] args)
         {
-            //string secretWord = "salam";
-            //string guess = "";
-            //int guessCount = 0;
-            //int guessLimit = 3;
-            //bool outOfGuesses = false;
 
-            //while (guess !=secretWord && !outOfGuesses)
-            //{
-            //    if (guessCount<guessLimit)
-            //    {
-            //        Console.WriteLine("Enter guess: ");
-            //        guess = Console.ReadLine();
-            //        guessCount++;
-            //    }
-            //    else
-            //    {
-            //        outOfGuesses = true;
-            //    }
-
-            //}
-            //if (outOfGuesses)
-            //{
-            //    Console.WriteLine("You Lose!");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You Win");
-            //}
-
-            //if (guessCount==1)
-            //{
-            //    Console.WriteLine("Skor: 6");
-            //}
-            //else if (guessCount == 2)
-            //{
-            //    Console.WriteLine("Skor: 4");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Skor: 2");
-            //}
-
-            //Console.WriteLine($"Yoxlanilan sozlerin sayi: {guessCount}");
+            aTmr.Elapsed += ATmr_Elapsed;
+            aTmr.Enabled = true;
+            aTmr.AutoReset = true;
+            aTmr.Start();
 
 
-            //int guess = 0;
-            //int guessCount = 0;
-            //int secretNumber;
-
-            //Random random = new Random();
-            //secretNumber = random.Next(1, 10);
-
-            //while (guess != secretNumber)
-            //{
-            //    Console.WriteLine("Enter the number: ");
-            //    guess = Convert.ToInt32(Console.ReadLine());
-            //    guessCount++;
-
-            //    if (guess>secretNumber)
-            //    {
-            //        Console.WriteLine("Little number");
-            //    }
-            //    else if (guess<secretNumber)
-            //    {
-
-            //        Console.WriteLine("Big number");
-
-            //    }        
-            //}
-            //Console.WriteLine("Congrulation !");
-            //Console.WriteLine("{0} find the number ",guessCount);
-
-
-
-
-            //string words = "This is really fun. I am learning to code. ";
-
-            //char[] list = new char[] { ' ', '.' };
-
-            //Random random = new Random();
-
-
-            //foreach (var item in words.Split(list, StringSplitOptions.RemoveEmptyEntries))
-            //{
-            //    Console.WriteLine(item.ToCharArray());
-            //    item.ToCharArray();
-            //    Console.WriteLine(item);
-            //    Console.WriteLine(item[random.Next(0, item.Length)]);
-            //}
-
-
-
-            //Random random = new Random();
-
-            //string[] words = { "man", "rat", "cow", "chicken" };
-
-            //string word = words[random.Next(0, words.Length)];
-
-            //Console.WriteLine(words);
-
-            string content = "This is really fun. I am learning to code.  ";
+            string content = "This is Really fun. I am Learning to code.  ";
             char[] list = new char[] { ' ', '.' };
 
-            var split = content.Split(list, StringSplitOptions.RemoveEmptyEntries);
+            var split = content.ToLower().Split(list, StringSplitOptions.RemoveEmptyEntries);
 
 
+        START:
             Random random = new Random();
-            string randomString = split[random.Next(0, split.Length)];
-            Console.WriteLine(randomString);
+
+            int i = random.Next(split.Length);
+
+
+
+            Console.WriteLine("\n" + split[i]);
+
+            string secretWord;
+            if (split[i] == "code")
+            {
+                secretWord = "code";
+            }
+            else
+            {
+                secretWord = split[i + 1];
+            }
+
+            int scor = 0;
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter guess:");
+                    guess = Console.ReadLine().ToLower();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+
+
+
+
+
+            if (guessCount == 1 && outOfGuesses == false)
+            {
+                correctCount++;
+                scor = +6;
+            }
+            else if (guessCount == 2 && outOfGuesses == false)
+            {
+                correctCount++;
+                scor = +4;
+            }
+            else if (guessCount == 3 && outOfGuesses == false)
+            {
+                correctCount++;
+                scor += 2;
+            }
+            else
+            {
+                scor += 0;
+            }
+            total += scor;
+
+
+
+
+            Console.WriteLine("\nContinue ? (Y/N)");
+            Console.WriteLine("\n");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                goto START;
+            }
+            else
+            {
+                Console.WriteLine("\nTotal points earned: " + total);
+                Console.WriteLine("The number of straight word pairs found: " + correctCount);
+                Console.WriteLine("Total number of checked word pairs: " + guessCount);
+            }
+
+            Console.ReadKey();
 
 
 
         }
+        private static void ATmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            secondsCount++;
+            //Console.WriteLine(secondsCount + " Seconds"); 
+            if (secondsCount == 20)  //20 second
+            {
+                Console.WriteLine("\nTotal points earned: " + total);
+                Console.WriteLine("The number of straight word pairs found: " + correctCount);
+             
+                System.Environment.Exit(0);         
+            }
 
+
+
+        }
 
     }
 }
