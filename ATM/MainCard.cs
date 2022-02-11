9f7359dbc08d356bf2dd6b467076f170868ee4fa
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-11 22:06:40 +0400

    update

---
+++ ATM/MainCard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Card
+{
+    class MainCard
+    {
+        public int ID { get; set; }
+        public long MainCardNumber { get; set; }
+        public int MainAmount { get; set; }
+        public DateTime ExperitionTime { get; set; }
+    
+        public static int id;
+
+        public MainCard()
+        {
+            ID = ++id;
+        }
+
+
+    }
+
+    class VirtualCard
+    {
+        public int ID { get; set; }
+        public long VirtualCardNumber { get; set; }
+        public int VirtualAmount { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+      
+        public static int id;
+
+        public VirtualCard()
+        {
+            ID = ++id;
+        }
+
+       
+    }
+
+
+}
--- Virtual Card/Program.cs
+++ Virtual Card/Program.cs
@@ -4,23 +4,41 @@ namespace Virtual_Card
 {
     class Program
     {
+        public static int amount = 500;
         static void Main(string[] args)
         {
-            int amount = 500;
+            
             int deposit;
             int withdraw;
-            int choose, pin = 0;
-            Console.WriteLine("Your pin number: ");
-            pin = int.Parse(Console.ReadLine());
+            int choose;
+         
+
+            bool opp = true;
+            while (opp)
+            {
+                Console.WriteLine("Pin kod 5 reqemli: ");
+                int cardNumber = int.Parse(Console.ReadLine());
+
+                if (cardNumber > 10000 && cardNumber < 99999)
+                {
+                    Console.WriteLine("Kecdi");
+                    opp = false;
+                }
+                else
+                {
+                    Console.WriteLine("Say duz deyil");
+                }
+            }
 
             while (true)
             {
-                Console.WriteLine("A T M \n");
+                Console.WriteLine("Main Card \n");
 
                 Console.WriteLine("1.Check Balance \n");
                 Console.WriteLine("2.Withdraw \n");
                 Console.WriteLine("3.Deposit \n");
-                Console.WriteLine("4.Exit \n");
+                Console.WriteLine("4.Create Card \n");
+                Console.WriteLine("5.Exit \n");
 
                 Console.WriteLine("Select option: ");
 
@@ -28,42 +46,87 @@ namespace Virtual_Card
                 switch (choose)
                 {
                     case 1:
-                        Console.WriteLine($"\n\n\nYour balance:{amount}");
+                        Console.WriteLine($"\n\n\nSenin balans:{amount}");
                         break;
 
                     case 2:
-                        Console.WriteLine("Amount to be withdrawed");
+                        Console.WriteLine("Cekilecek mebleg");
                         withdraw = int.Parse(Console.ReadLine());
-                        if (withdraw %100 !=0)
+                        if (withdraw % 100 != 0)
                         {
-                            Console.WriteLine("\n\nEnter the amount in Multiple of 100");
+                            Console.WriteLine("\n\nMəbləgi 100-ə qeder daxil edin");
                         }
-                        else if (withdraw >(amount-50))
+                        else if (withdraw > (amount - 30))
                         {
-                            Console.WriteLine("\n\nInsufficient Fund");
+                            Console.WriteLine("\n\nQeyri-kafi miqdar");
                         }
                         else
                         {
                             amount -= withdraw;
-                            Console.WriteLine("\n\n Take your Money");
-                            Console.WriteLine($"\n Current balance is: {amount}");
+                            Console.WriteLine("\n\nPulu gotur");
+                            Console.WriteLine($"\nIndiki balans: {amount}");
                         }
                         break;
 
                     case 3:
-                        Console.WriteLine("\n\nEnter the amount to be deposited");
+                        Console.WriteLine("\n\nDepositi artir");
                         deposit = int.Parse(Console.ReadLine());
                         amount += deposit;
                         break;
 
                     case 4:
-                        Console.WriteLine("\n\nThank!!!");
+                        CreateCard();
+                        break;
+
+
+                    case 5:
+                        Console.WriteLine("\n\nTesekkurler!!!");
                         System.Environment.Exit(0);
                         break;
                     default:
                         break;
                 }
-            }
+            }          
         }
+
+       static void CreateCard()
+        {
+            bool op = true;
+            while (op)
+            {
+                Console.WriteLine("Kart nomresi 5 reqemli: ");
+                int cardNumber = int.Parse(Console.ReadLine());
+
+                if (cardNumber > 10000 && cardNumber < 99999)
+                {
+                    Console.WriteLine("Kecdi");
+                    op = false;
+                }
+                else
+                {
+                    Console.WriteLine("Say duz deyil");
+                }
+
+            }
+
+
+            Console.WriteLine("Yeni kart ucun mebleg: ");
+            int virtualAmount = int.Parse(Console.ReadLine());
+
+
+            if (virtualAmount > amount)
+            {
+                Console.WriteLine("Esas kartdaki meblegden, cox ola bilmez");
+            }
+            else
+            {
+                amount -= virtualAmount;
+                Console.WriteLine($"Qalan mebleg: {amount}");
+            }
+
+
+
+        }
+
     }
 }
