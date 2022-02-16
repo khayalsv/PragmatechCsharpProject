bc9159378df79866ab2c631ac6f52b738a480a0a
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-16 16:43:34 +0400

    update

---
+++ Algorithms/4angles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4angles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Width: ");
+            int width=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
