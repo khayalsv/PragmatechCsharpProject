2ac516adc2012f2938c3f54a0d23a28807be7a05
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-02-18 02:15:04 +0400

    update

---
+++ AspNet/MVCProjectCamp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCProjectCamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
