32614f48b2796cf997e002d0bc1091326c5ace6d
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-03-16 16:43:57 +0400

    update

--- AspNet/NoTech/Areas/Admin/Controllers/CounterController.cs
+++ AspNet/NoTech/Areas/Admin/Controllers/CounterController.cs
@@ -98,5 +98,35 @@ namespace NoTech.Areas.Admin.Controllers
             return Redirect("/Admin/Counter/List");
 
         }
+
+        //AJAX
+        [HttpPost]
+        public async Task<JsonResult> CreateAjax(string odometer, string title, string text, string icon)
+        {
+            if (odometer== null && title == null && text == null && icon == null)
+            {
+                return Json(new
+                {
+                    status = 400
+                });
+            }
+
+            var item = new Counter()
+            {
+                Odometer = odometer,
+                Title = title,
+                Text = text,
+                Icon = icon
+            };
+
+            await myContext.Counters.AddAsync(item);
+            await myContext.SaveChangesAsync();
+
+            return Json(new
+            {
+                status = 200
+            });
+        }
+      
     }
 }
