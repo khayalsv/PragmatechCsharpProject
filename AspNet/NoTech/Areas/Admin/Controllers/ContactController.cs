665c04db30a826572dfcd7d9e70666906f93ba69
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-03-19 01:40:16 +0400

    update

--- AspNet/NoTech/Areas/Admin/Controllers/ContactController.cs
+++ AspNet/NoTech/Areas/Admin/Controllers/ContactController.cs
@@ -26,53 +26,6 @@ namespace NoTech.Areas.Admin.Controllers
             return View(item);
         }
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            var item = new Contact();
-            return View(item);
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> Create(Contact item)
-        {
-            if (!ModelState.IsValid) return View(item); //datanin olmadigini yoxluyur
-
-
-            await myContext.Contacts.AddAsync(item);
-            await myContext.SaveChangesAsync();
-
-            return Redirect("List");
-        }
-
-
-        [HttpGet]
-        public IActionResult Edit(int? id)
-        {
-            if (id == null) return NotFound();
-
-            var item = myContext.Contacts.Find(id);
-
-            if (item == null) return NotFound();
-
-            return View(item);
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(Contact item)
-        {
-            if (!ModelState.IsValid) return View(item);
-
-            var list = myContext.Contacts.Find(item.ID);
-            list.Name = item.Name;
-            list.Email = item.Email;
-            list.Message = item.Message;
-
-            await myContext.SaveChangesAsync();
-            return Redirect("/Admin/Contact/List");
-        }
 
         [HttpGet]
         public async Task<IActionResult> Delete(int? id)
