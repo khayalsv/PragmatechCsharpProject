02f38b90358f79354d8513c735e963ef1a54b19c
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-03-25 18:24:22 +0400

    update

--- AspNet/NoTech/Areas/Admin/Controllers/CounterController.cs
+++ AspNet/NoTech/Areas/Admin/Controllers/CounterController.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 namespace NoTech.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
+    [Authorize(Roles = "Admin")]
     public class CounterController : Controller
     {
         private readonly MyContext myContext;
