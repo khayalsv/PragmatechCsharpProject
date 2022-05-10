8a18abc6023c89f71a281f27a81ebc1da8171bd7
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-05-10 14:19:38 +0400

    update

---
+++ AspNet/BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void Add(Blog item);
+        void Delete(Blog item);
+        void Update(Blog item);
+        List<Blog> GetAllList();
+        Blog GetById(int id);
+    }
+}
---
+++ AspNet/BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void Add(Blog item)
+        {
+            _blogDal.Insert(item);
+        }
+
+        public void Delete(Blog item)
+        {
+            _blogDal.Delete(item);
+        }
+
+        public List<Blog> GetAllList()
+        {
+            return _blogDal.GetAllList();
+        }
+
+        public Blog GetById(int id)
+        {
+            return _blogDal.GetById(id);
+        }
+
+        public void Update(Blog item)
+        {
+            _blogDal.Update(item);
+        }
+    }
+}
