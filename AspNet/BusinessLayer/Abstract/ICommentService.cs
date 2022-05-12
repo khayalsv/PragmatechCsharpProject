9fd1c8dea3a3521476c1581f361e3893134edd80
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-05-13 00:34:24 +0400

    update

---
+++ AspNet/BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICommentService
+    {
+        void Add(Comment item);
+       // void Delete(Comment item);
+       // void Update(Comment item);
+        List<Comment> GetAllList(int id);
+       // Comment GetById(int id);
+    }
+}
---
+++ AspNet/CoreDemo/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager cm = new CommentManager(new EfCommentyRepository());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult CommentListByBlog(int id)
+        {
+            var values = cm.GetAllList(id);
+            return PartialView(values);
+        }
+    }
+}
