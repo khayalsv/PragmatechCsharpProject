7a04144ec05aa75479012c893f4639735bc3b02e
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-05-13 22:24:51 +0400

    update

---
+++ AspNet/CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog:ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentyRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetAllList(2);
+            return View(values);
+        }
+    }
+}
