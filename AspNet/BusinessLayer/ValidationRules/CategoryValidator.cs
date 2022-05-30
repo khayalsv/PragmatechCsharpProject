6c640fedb89e6c96f1d3e91ac0f8d99671402792
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-05-31 02:18:27 +0400

    update

---
+++ AspNet/BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad bos ola bilmez");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Ad bos ola bilmez");
+            RuleFor(x => x.Name).Length(2,40).WithMessage("Uzunlugu duzgun qeyd edin");
+
+        }
+    }
+}
---
+++ AspNet/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        MyContext _myContext = new MyContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = _myContext.Contacts.Count();
+            ViewBag.v3 = _myContext.Comments.Count();
+
+            return View();
+        }
+    }
+}
