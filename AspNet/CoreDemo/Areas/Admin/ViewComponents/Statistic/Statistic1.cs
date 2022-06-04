12da94d72a16ebcd1e36d695c0b246e6537db658
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-06-04 15:57:01 +0400

    update

--- AspNet/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ AspNet/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = _myContext.Contacts.Count();
             ViewBag.v3 = _myContext.Comments.Count();
+
+            string api = "a2c6d84a04a93ea1624a00da1f0b99fe";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Sumgait&mode=xml&lang=az=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
 
             return View();
         }
---
+++ AspNet/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,21 @@
+﻿
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        MyContext _myContext = new MyContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _myContext.Blogs.OrderByDescending(x => x.Id).Select(x=>x.Title).Take(1).FirstOrDefault();
+
+            return View();
+        }
+    }
+}
