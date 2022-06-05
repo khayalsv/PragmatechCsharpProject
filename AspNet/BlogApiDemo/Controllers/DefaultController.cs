265fab9f33bc61f6c019d76c2f3db8c13838ffc5
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2022-06-05 15:58:26 +0400

    update

---
+++ AspNet/BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,80 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var _myContext = new MyContext();
+            var values = _myContext.Employees.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult EmployeeAdd(Employee employee)
+        {
+            using var _myContext = new MyContext();
+            _myContext.Add(employee);
+            _myContext.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var _myContext = new MyContext();
+            var employee = _myContext.Employees.Find(id);
+            if (employee == null)
+                return NotFound();
+            else
+                return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var _myContext = new MyContext();
+            var employee = _myContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _myContext.Remove(employee);
+                _myContext.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult EmployeeUpdate(Employee item)
+        {
+            using var _myContext = new MyContext();
+            var employee = _myContext.Find<Employee>(item.Id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                employee.Name = item.Name;
+                _myContext.Update(employee);
+                _myContext.SaveChanges();
+                return Ok();
+            }
+
+        }
+    }
+}
---
+++ AspNet/BlogApiDemo/DataAccessLayer/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SALIMOV\\SQLEXPRESS;Database=CoreBlogApiDemo;Trusted_Connection=True;MultipleActiveResultSets=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
